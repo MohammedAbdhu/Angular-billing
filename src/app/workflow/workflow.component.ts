9c8413cd039df78c4f2f7434bec679123e58cfd7
mohammed <abdullah9160@outlook.com>
2025-03-04 09:45:22 +0530

    side nav updated

--- src/app/workflow/workflow.component.ts
+++ src/app/workflow/workflow.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { MaterialModule } from '../shared/common/material.module';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-workflow',
-  imports: [MaterialModule],
+  imports: [CommonModule, MaterialModule],
   standalone: true,
   templateUrl: './workflow.component.html',
   styleUrl: './workflow.component.scss'
 })
 export class WorkflowComponent {
   showFiller = false;
+  sideNavList = [
+    {icon: 'dashboard', name: 'Dashboard', url: ''},
+    {icon: 'list', name: 'Billing', url: ''},
+    {icon: 'supervisor_account', name: 'Customers', url: ''}
+  ]
 }
